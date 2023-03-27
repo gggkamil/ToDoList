cf196cd1fef9ec84566526f5c4811a09cca53a69
Kamil Dębicki <74270259+gggkamil@users.noreply.github.com>
2023-03-27 22:22:00 +0200

    Login

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -4,5 +4,9 @@ namespace ToDoList.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        class private Login(char login)
+        {
+            _login=login;
+        }
     }
 }
